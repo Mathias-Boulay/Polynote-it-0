1ce66e40b9a4462bc37f1a279f27d7be4d4403fe
Mathias-Boulay <mathiasboulay@free.fr>
2023-05-02 16:03:46 +0200

    Build: Handle more env vars

--- backend/polynote/src/constants.ts
+++ backend/polynote/src/constants.ts
@@ -5,9 +5,11 @@ export const jwtConstants = {
 
 export const COOKIE_SECRET = process.env.COOKIE_SECRET || 'LOCAL_HOST_SECRET';
 
-const MONGO_USERNAME = 'ADMIN_USERNAME';
-const MONGO_PASSWORD = 'R43etx50qCDqJpnpzWPs';
-export const MONGO_CONNECTION_STRING = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:27017`;
+// You know, the default values should be used for local dev only
+const MONGO_USERNAME = process.env.MONGO_USERNAME || 'ADMIN_USERNAME';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'R43etx50qCDqJpnpzWPs';
+const MONGO_HOST = process.env.MONGO_HOST || 'db';
+export const MONGO_CONNECTION_STRING = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:27017`;
 
 /** Email related constants */
 export const MAIL_SENDER = process.env.MAIL_SENDER || 'Polynotes services';
